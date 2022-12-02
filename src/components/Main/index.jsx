b4b829e778e22f4fc993565a0e51347dd894f35a
Shoba <shobamayidd@gmail.com>
2022-12-02 13:16:55 -0300

    Inicio de pagina web ejemplo

---
+++ src/components/Main/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { About } from '../About';
+import { Hero } from '../Hero';
+import { Skills } from '../Skills';
+
+const Main = () => {
+    return(
+        <div>
+       
+        <Hero/>
+        <About/>
+        <Skills/>
+        </div>
+    )
+}
+
+export { Main }
